293909fdec94a83abfa2aa111096734c3bb4a75f
League <league@iMac-96.attlocal.net>
2019-05-05 12:59:11 -0700

    module 4
    :sunglasses:

---
+++ Googly_Eyes/Googly_Eyes.pde
@@ -0,0 +1,20 @@
+PImage face;
+void setup(){
+  face= loadImage("resting-bitchface.jpg");
+  size(800,600);
+  face.resize(width,height);
+  background(face);
+}
+void draw(){
+  fill(255,255,255);
+  ellipse(375,250,75,75);
+  ellipse(425,250,75,75);
+  fill(0,0,0);
+  if(mouseX>725&&
+  for(int i=0; i<100;i++){
+   
+  ellipse(mouseX+25,mouseY+25,35,35);
+  ellipse(mouseX+75,mouseY+25,35,35);
+  background(face);
+  }       
+}
--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -10,37 +10,47 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		String pet=JOptionPane.showInputDialog("What kind of pet do you want to buy");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
+			for(int i=0;i<3;i++) {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Give food", "Give water", "Groom" }, null);
-System.out.println(task);
-			// 5. Use user input to call the appropriate method created in step 4.
 
+			// 5. Use user input to call the appropriate method created in step 4.
+			if(task==0) {
+				giveFood(pet);
+			}
+			else if(task==1) {
+				giveWater(pet);
+			}
+			else if(task==2) {
+				groom(pet);
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 			if (happinessLevel>10) {
 				JOptionPane.showMessageDialog(null,"You love your dog!");
-			
+				break;
+			}
 			}
 			
 	}
+	
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
-	void giveWater() {
+	static void giveWater(String str) {
 		happinessLevel=happinessLevel+2;
-		JOptionPane.showMessageDialog(null, "Your "+pet+" slurped");
+		JOptionPane.showMessageDialog(null, "Your "+str+" slurped");
 	}
-	void giveFood() {
-		happinessLevel=happinessLevel+2;
-		JOptionPane.showMessageDialog(null, "Your "+pet+" slurped");
+	static void giveFood(String str) {
+		happinessLevel=happinessLevel+4;
+		JOptionPane.showMessageDialog(null, "Your "+str+" chewed");
 	}
-	void groom() {
-		happinessLevel=happinessLevel+2;
-		JOptionPane.showMessageDialog(null, "Your "+pet+" slurped");
+	static void groom(String str) {
+		happinessLevel=happinessLevel+6;
+		JOptionPane.showMessageDialog(null, "Your "+str+" relaxed");
 	}
 }
